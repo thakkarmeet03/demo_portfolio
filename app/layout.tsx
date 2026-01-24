49e68dfa36445e19390aff4e5d576b88616fec8d
Meet Thakkar <meet7tha@gmail.com>
2026-01-24 08:01:33 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         >
             {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
